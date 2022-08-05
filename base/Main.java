0918687f250a2e934c30a61e6209bdfb584a2fc2
simodake <simodake@gmail.com>
2022-08-05 19:56:26 +0900

    FastScanner

--- base/Main.java
+++ base/Main.java
@@ -1,60 +1,152 @@
 import java.util.*;
-import java.util.stream.Collectors;
 import java.io.*;
 import java.util.regex.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), q = sc.nextInt();
-        UnionFind unionFind = new UnionFind(n + 1);
-
-        while(q-- > 0) {
-            int p = sc.nextInt(), a = sc.nextInt(), b = sc.nextInt();
-            if(p == 0) {
-                unionFind.unite(a, b);
-            } else {
-                System.out.println(unionFind.same(a, b) ? "Yes" : "No");
-            }
-        }
+    public static void main(String[] args) {
+        FScanner sc = new FScanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        solve(sc, out);
+        out.flush();
         sc.close();
     }
-
+    
+    public static void solve(FScanner sc, PrintWriter out) {
+        int n = sc.nextInt();
+        int cnt = 0;
+        int a = 0, b = 0;
+        Pattern p = Pattern.compile("AB");
+        while(n-- > 0) {
+            String s = sc.next();
+            Matcher m = p.matcher(s);
+            int index = 0;
+            while(m.find(index)) {
+                cnt++;
+                index = m.end();
+            }
+            if(s.startsWith("B")) {
+                b++;
+            }
+            if(s.endsWith("A")) {
+                a++;
+            }
+        }
+        System.out.println(cnt + Math.min(Math.min(a, b), n - 1));
+    }
 }
 
 
-class UnionFind {
-    public int[] parents, counts;
-    public UnionFind(int length) {
-        this.parents = new int[length];
-        this.counts = new int[length];
-        for(int i = 0; i < length; i++) {
-            parents[i] = i;
+class FScanner {
+    private InputStream in;
+    private final byte[] buffer = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    FScanner(InputStream in) {
+        this.in = in;
+    }
+
+    private boolean hasNextByte() {
+        if (ptr < buflen) {
+            return true;
+        } else {
+            ptr = 0;
+            try {
+                buflen = in.read(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (buflen <= 0) {
+                return false;
+            }
         }
-        Arrays.fill(counts, 1);
+        return true;
     }
 
-    public int root(int x) {
-        int tmp = x;
-        while(tmp != parents[tmp]) {
-            tmp = parents[tmp];
+    private int readByte() {
+        if (hasNextByte())
+            return buffer[ptr++];
+        else
+            return -1;
+    }
+
+    private static boolean isPrintableChar(int c) {
+        return 33 <= c && c <= 126;
+    }
+
+    private void skipUnprintable() {
+        while (hasNextByte() && !isPrintableChar(buffer[ptr]))
+            ptr++;
+    }
+
+    public boolean hasNext() {
+        skipUnprintable();
+        return hasNextByte();
+    }
+
+    public String next() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        StringBuilder sb = new StringBuilder();
+        int b = readByte();
+        while (isPrintableChar(b)) {
+            sb.appendCodePoint(b);
+            b = readByte();
         }
-        return parents[x] = tmp;
+        return sb.toString();
     }
 
-    public void unite(int x, int y) {
-        int rootX = root(x);
-        int rootY = root(y);
-        if (rootX == rootY) return;
-        counts[rootY] += counts[rootX];
-        parents[rootX] = rootY;
-    }
-    
-    public boolean same(int x, int y) {
-        return root(x) == root(y);
+    public int nextInt() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        int n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (b != -1 && isPrintableChar(b)) {
+            if ('0' <= b && b <= '9') {
+                n *= 10;
+                n += b - '0';
+            } else {
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+        return minus ? -n : n;
     }
 
-    public int count(int x) {
-        return counts[root(x)];
+    public long nextLong() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        long n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (b != -1 && isPrintableChar(b)) {
+            if ('0' <= b && b <= '9') {
+                n *= 10;
+                n += b - '0';
+            } else {
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+        return minus ? -n : n;
+    }
+
+    public boolean close() {
+        return true;
     }
 }
--- prime/Main.java
+++ prime/Main.java
@@ -3,15 +3,19 @@ import java.util.stream.Collectors;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Arrays.asList(br.readLine().split(" ")).stream().map(Long::parseLong).forEach(n -> {
-            System.out.println(Prime.factorize(n).entrySet().stream().map(entry -> {
-                return entry.getKey() + ":" + entry.getValue();
-            }).collect(Collectors.joining(",")));
-        });
+    public static void main(String[] args) {
+        FScanner sc = new FScanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        solve(sc, out);
+        out.flush();
+        sc.close();
     }
-
+    
+    public static void solve(FScanner sc, PrintWriter out) {
+        out.println(Prime.factorize(sc.nextLong()).entrySet().stream().map(entry -> {
+            return entry.getKey() + ":" + entry.getValue();
+        }).collect(Collectors.joining(",")));
+    }
     public static class Prime {
         private static long initialized = 0;
         private static Map<Long, LinkedHashMap<Long, Integer>> memo = new HashMap<>();
@@ -56,5 +60,120 @@ public class Main {
             }
             return memo.get(n);
         }
+    }
+}
+
+class FScanner {
+    private InputStream in;
+    private final byte[] buffer = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    FScanner(InputStream in) {
+        this.in = in;
+    }
+
+    private boolean hasNextByte() {
+        if (ptr < buflen) {
+            return true;
+        } else {
+            ptr = 0;
+            try {
+                buflen = in.read(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (buflen <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int readByte() {
+        if (hasNextByte())
+            return buffer[ptr++];
+        else
+            return -1;
+    }
+
+    private static boolean isPrintableChar(int c) {
+        return 33 <= c && c <= 126;
+    }
+
+    private void skipUnprintable() {
+        while (hasNextByte() && !isPrintableChar(buffer[ptr]))
+            ptr++;
+    }
+
+    public boolean hasNext() {
+        skipUnprintable();
+        return hasNextByte();
+    }
+
+    public String next() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        StringBuilder sb = new StringBuilder();
+        int b = readByte();
+        while (isPrintableChar(b)) {
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+
+    public int nextInt() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        int n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (b != -1 && isPrintableChar(b)) {
+            if ('0' <= b && b <= '9') {
+                n *= 10;
+                n += b - '0';
+            } else {
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+        return minus ? -n : n;
+    }
+
+    public long nextLong() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+        long n = 0;
+        boolean minus = false;
+        int b = readByte();
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+        if (b < '0' || '9' < b) {
+            throw new NumberFormatException();
+        }
+        while (b != -1 && isPrintableChar(b)) {
+            if ('0' <= b && b <= '9') {
+                n *= 10;
+                n += b - '0';
+            } else {
+                throw new NumberFormatException();
+            }
+            b = readByte();
+        }
+        return minus ? -n : n;
+    }
+
+    public boolean close() {
+        return true;
     }
 }
